4e19664b85ab9da2ae32051bafb7702e7e647781
Zion Dionisio <zndionisio.2121@gmail.com>
2025-03-22 17:57:00 +0800

    Keymap to close additional windows, silenced some keymaps

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,40 +1,37 @@
 local key = vim.keymap
 
--- Return to Oil.nvim
-key.set("n", "<leader>ee", "<CMD>Oil<CR>")
+key.set("n", "<leader>ee", "<CMD>Oil<CR>", { desc = "Return to Oil.nvim", silent = true })
 
 -- Write file
-key.set("n", "<leader>ww", "<CMD>w<CR>")
-key.set("n", "<leader>wq", "<CMD>wq<CR>")
-key.set("n", "<leader>qq", "<CMD>q!<CR>")
+key.set("n", "<leader>ww", "<CMD>w<CR>", { desc = "Write/Save File" })
+key.set("n", "<leader>wq", "<CMD>wq<CR>", { desc = "Write file then quit" })
+key.set("n", "<leader>qa", "<CMD>qa!<CR>", { desc = "Force quit without saving", silent = true })
+key.set("n", "<leader>qw", "<CMD>close<CR>", { desc = "Close window", silent = true })
 
--- Select the whole file
-key.set("n", "<leader><C-a>", "ggvG")
-
--- Quit
-key.set("n", "<leader>qq", "<CMD>q<CR>")
+key.set("n", "<leader><C-a>", "ggvG", { desc = "Select the entire buffer", silent = true })
 
 -- Move text up and down
-key.set("v", "K", ":m '<-2<CR>gv=gv")
-key.set("v", "J", ":m '>+1<CR>gv=gv")
+key.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected text up", silent = true })
+key.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected text down", silent = true })
 
 -- Center cursor while navigating vertically
-key.set("n", "<C-u>", "<C-u>zzzv")
-key.set("n", "<C-d>", "<C-d>zzzv")
+key.set("n", "<C-u>", "<C-u>zzzv", { desc = "Center cursor while PgUp", silent = true })
+key.set("n", "<C-d>", "<C-d>zzzv", { desc = "Center cursor while PgDown", silent = true })
 
 -- Center cursor while searching terms
-key.set("n", "n", "nzzzv")
-key.set("n", "N", "Nzzzv")
+key.set("n", "n", "nzzzv", { desc = "Center cursor in forward incremental search", silent = true })
+key.set("n", "N", "Nzzzv", { desc = "Center cursor in backward incremental search", silent = true })
+
+-- Delete with void register
+key.set({ "n", "v" }, "d", '"_d', { desc = "Delete to the void register" })
 
 -- Delete previous word in insert mode
-key.set("i", "<C-H>", "<ESC>ciw")
-
--- Delete with the void register
-key.set({ "n", "v" }, "d", '"_d')
+key.set("i", "<C-H>", '<C-o>"_diw', { desc = "Delete the current word in insert mode" })
 
 -- Copy to the system clipboard
-key.set("v", "<C-S-Y>", '"+yy')
+key.set("v", "<C-S-Y>", '"+yy', { desc = "Copy to system clipboard", silent = false })
 
+-- Incremental Rename
 key.set("n", "<leader>rn", function()
 	return ":IncRename " .. vim.fn.expand("<cword>")
 end, { expr = true })
@@ -52,8 +49,8 @@ key.set("n", "<leader>fw", builtin.live_grep, { desc = "Telescope live grep" })
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-key.set("n", "<leader>hh", mark.add_file)
-key.set("n", "<C-h>", ui.toggle_quick_menu)
+key.set("n", "<leader>hh", mark.add_file, { desc = "Add mark to harpoon" })
+key.set("n", "<C-h>", ui.toggle_quick_menu, { desc = "Open harpoon UI" })
 
 key.set("n", "<leader>1", function()
 	ui.nav_file(1)
@@ -74,7 +71,7 @@ key.set("n", "<leader>/", "<CMD>lua require('searchbox').match_all({title='Searc
 key.set("n", "?", vim.cmd.SearchBoxReplace)
 
 -- Undotree
-key.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+key.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree pane" })
 
 -- Peek
 local peek = require("peek")
@@ -84,6 +81,10 @@ key.set("n", "<leader>po", function()
 	else
 		peek.open()
 	end
-end)
+end, { desc = "Toggle Markdown Preview" })
 
-key.set("n", "<leader>lt", vim.cmd.LiveServerToggle)
+-- Live Server
+key.set("n", "<leader>lt", vim.cmd.LiveServerToggle, { desc = "Toggle Live Server" })
+
+-- Transparent
+key.set("n", "<leader>z", vim.cmd.TransparentToggle, { desc = "Toggle background opacity" })
