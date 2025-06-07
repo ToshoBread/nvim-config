948241fc6a85494895f0e498aca512103d0e80d9
Zion Dionisio <zndionisio.2121@gmail.com>
2025-06-07 19:19:28 +0800

    Removed(Not Working): github-preview Tweaked: Lualine & Noice

--- lua/plugins/preview.lua
+++ lua/plugins/preview.lua
@@ -3,19 +3,17 @@ return {
 	dependencies = {
 		{ "chomosuke/typst-preview.nvim", lazy = true }, -- typst
 		{ "toppair/peek.nvim", lazy = true, build = "deno task --quiet build:fast" }, -- markdown
-		{ "wallpants/github-preview.nvim", lazy = true }, -- Github READMEs
-		-- { 'hat0uma/csvview.nvim',         lazy = true },                                         -- csv
+		{ "hat0uma/csvview.nvim", lazy = true }, -- csv
 	},
 	config = function()
 		require("omni-preview").setup()
+
 		require("typst-preview").setup()
 		require("peek").setup({ app = "browser" })
-		require("github-preview").setup({})
-
-		-- require("csvview").setup({
-		--     view = {
-		--         display_mode = "border"
-		--     }
-		-- })
+		require("csvview").setup({
+			view = {
+				display_mode = "border",
+			},
+		})
 	end,
 }
