2cf53a290a8a106931b85f79118498369ba0d2fa
Zion Dionisio <93123377+ToshoBread@users.noreply.github.com>
2025-07-18 20:33:40 +0800

    Tweaked some settings and added globals file for vars and funcs

---
+++ lua/core/globals.lua
@@ -0,0 +1,13 @@
+-- Auto set noremap and silent on remaps, allowing for config
+function _G.remap(mode, lhs, rhs, opts)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+-- Temporarily change colorscheme
+function _G.changeTheme(color)
+	vim.cmd.colorscheme(color)
+end
