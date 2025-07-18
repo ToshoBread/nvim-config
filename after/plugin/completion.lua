b49f36bf319546673a437ff0608a9683ea3ca0af
Zion Dionisio <93123377+ToshoBread@users.noreply.github.com>
2025-07-18 20:33:40 +0800

    Configured UI and QoL plugins, Tweaked some other existing configs

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -1,71 +1,87 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 require("luasnip.loaders.from_vscode").lazy_load()
 
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
 cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-        end,
-    },
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
 
-    completion = {
-        completeopt = "menu,menuone,noinsert",
-    },
+	completion = {
+		completeopt = "menu,menuone,noinsert",
+	},
 
-    performance = {
-        debounce = 50,
-        throttle = 50,
-        fetching_timeout = 200,
-        max_item_count = 10,
-    },
+	performance = {
+		debounce = 50,
+		throttle = 50,
+		fetching_timeout = 200,
+		max_item_count = 30,
+	},
 
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
 
-    mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<C-a>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-s>"] = cmp.mapping.scroll_docs(4),
-    }),
+	mapping = cmp.mapping.preset.insert({
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<C-a>"] = cmp.mapping.scroll_docs(-4),
+		["<C-s>"] = cmp.mapping.scroll_docs(4),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
 
-    sources = cmp.config.sources({
-        {
-            name = "nvim_lsp",
-            entry_filter = function(entry, ctx)
-                local kind = entry:get_kind()
+	sources = cmp.config.sources({
+		{
+			name = "nvim_lsp",
+			entry_filter = function(entry, ctx)
+				local kind = entry:get_kind()
 
-                if kind == cmp.lsp.CompletionItemKind.Text then
-                    return false
-                end
+				if kind == cmp.lsp.CompletionItemKind[1] then
+					return false
+				end
 
-                return true
-            end,
-        },
-        { name = "luasnip" },
-        { name = "path" },
-    }),
+				return true
+			end,
+		},
+		{ name = "luasnip" },
+		{ name = "nvim_lua" },
+		{ name = "path" },
+		{ name = "vim-dadbod-completion" },
+	}),
 })
 
 cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = "path" },
-        { name = "cmdline" },
-    }),
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+		{ name = "cmdline" },
+	}),
 })
 
 cmp.setup.cmdline("/", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = "buffer" },
-    },
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
 })
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,17 +1,17 @@
 require("telescope").setup({
-    defaults = {
-        border = true,
-        winblend = 0,
-        file_ignore_pattens = {
-            "node_modules",
-            ".git/",
-            "build",
-            "dist",
-        },
-    },
-    pickers = {
-        find_files = {
-            theme = "dropdown",
-        },
-    },
+	defaults = {
+		border = true,
+		winblend = 0,
+		file_ignore_pattens = {
+			"node_modules",
+			".git/",
+			"build",
+			"dist",
+		},
+	},
+	pickers = {
+		find_files = {
+			theme = "ivy",
+		},
+	},
 })
