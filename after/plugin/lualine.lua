bb392ab421e026c65be816d869543a57889738e6
Zion Dionisio <93123377+ToshoBread@users.noreply.github.com>
2025-07-18 20:33:40 +0800

    Section X now shows attached LSP

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -22,7 +22,7 @@ require("lualine").setup({
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = { "" },
-		lualine_x = { "" },
+		lualine_x = { "lsp_status", "searchcount" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
 	},
