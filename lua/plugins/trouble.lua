0bccb3ee4bc36f3fc93c42359feca515c822037d
Zion Dionisio <93123377+ToshoBread@users.noreply.github.com>
2026-05-28 19:34:38 +0800

    fix: remove cmd from trouble and vim-fugitive specs
    cmd acts as a lazy-load trigger in lazy.nvim, deferring
    plugin loading until the command is invoked. Remove cmd so
    both load at startup.
    
    Also re-add trouble setup() which was dropped in the opts
    to config migration.

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,13 +1,22 @@
 return {
 	{
 		"folke/trouble.nvim",
-		cmd = "Trouble",
-		opts = {},
 		config = function()
+			require("trouble").setup({})
 			Remap("n", "<leader>dx", "<cmd>Trouble diagnostics toggle<CR>", { desc = "Workspace Diagnostics" })
-			Remap("n", "<leader>dX", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", { desc = "Buffer Diagnostics" })
+			Remap(
+				"n",
+				"<leader>dX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+				{ desc = "Buffer Diagnostics" }
+			)
 			Remap("n", "<leader>ds", "<cmd>Trouble symbols toggle focus=false<CR>", { desc = "Document Symbols" })
-			Remap("n", "<leader>dl", "<cmd>Trouble lsp toggle focus=false win.position=right<CR>", { desc = "LSP Location List" })
+			Remap(
+				"n",
+				"<leader>dl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<CR>",
+				{ desc = "LSP Location List" }
+			)
 			Remap("n", "<leader>dL", "<cmd>Trouble loclist toggle<CR>", { desc = "Location List" })
 			Remap("n", "<leader>dQ", "<cmd>Trouble qflist toggle<CR>", { desc = "Quickfix List" })
 		end,
--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,7 +1,6 @@
 return {
 	{
 		"tpope/vim-fugitive",
-		cmd = "Git",
 		config = function()
 			Remap("n", "<leader>gt", "<cmd>tab Git<CR>", { desc = "Git stage window" })
 		end,
